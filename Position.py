ee10014dcfbca0b1dd59a437adae14f6c33ec8fd
Sadi <50300882+saadfareed@users.noreply.github.com>
2021-10-08 15:40:33 +0500

    Create Position.py

---
+++ Position.py
@@ -0,0 +1,12 @@
+def position(inde):
+    flag=False
+    sentence= "my name is Saad fareed. I am student of Computer Science. I am from Session 18 and Section A."
+    sentence=sentence.split(".")
+    #print(sentence)
+    for i in sentence:
+        #print(i)
+        if(inde==i):
+            flag=True
+            print(sentence.index(inde))
+    return flag
+print(position(" I am from Session 18 and Section A"))
